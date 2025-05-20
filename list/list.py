b51c3f1454c31e727efa621f7fe2d8455a36a3af
FarazAhmadKhann <farazahmadwork1@gmail.com>
2025-05-21 02:22:24 +0500

    9

--- list/list.py
+++ list/list.py
@@ -16,3 +16,39 @@ if "apple" in list:
 print(thislist)
 thislist.insert(1,"dryfruit")
 print(thislist)
+
+#add list items
+# 1- append 2- insert 3- extend
+city=["islamabad","karachi","lahore","multan","peshawar"]
+province=["Punjab","KPK","Balochistan","Sindh"]
+city.append("Rawalpindi")
+province.insert(1,"Kashmir")
+print(city)
+print(province)
+city.extend(province[1:3])
+print(city)
+
+
+# 1- remove("islamabad") 2- pop() 3- pop(1) 4- del thelist[2] 5- del thislist 6- thislist.clear()
+print("we doing delete now")
+del city[1:3]
+print(city)
+city.pop(0)
+print(city)
+city.clear()
+print(city)
+del city
+
+
+#1- list.sort() 2- list.sort(reverse=true) 3- list.reverse() 4-list.sort(key=func)
+print("we do sort now")
+number=[1,6,4,88,92,34,54,3,1]
+number.sort()
+print(number)
+number.sort(reverse=True)
+print(number)
+def func(n):
+ return abs(n-60)
+number.sort(key=func)
+print(number)
+
