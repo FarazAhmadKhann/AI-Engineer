14e44daaa7dd40cef502ed6785a04b45cb3ac4db
FarazAhmadKhann <farazahmadwork1@gmail.com>
2025-05-22 01:51:49 +0500

    11

--- loops/for.py
+++ loops/for.py
@@ -1,4 +1,4 @@
-a=6
+a=1000000
 for x in range(a):
   print(x)
   for y in range(6,0,-1):
@@ -12,3 +12,18 @@ for x in range(a):
     print("* ",end= "")
   print("")     
            
+
+n = 5  # Should be odd for symmetry
+stars = "* "
+spaces = " " * (n // 2)
+
+# Top half
+for i in range(1, n // 2 + 2):
+    print(spaces + stars * i)
+    spaces = spaces[:-1]
+
+# Bottom half
+spaces = " "  # Reset to one space
+for i in range(n // 2, 0, -1):
+    print(spaces + stars * i)
+    spaces += " "
